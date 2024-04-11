0d73f02f27839a649b15af39ff8f56fe3297a60b
yang.yang <yang.yang@vhd.com.cn>
2024-04-11 08:40:08 -0400

    opencv sdk

---
+++ NativeCapture/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+TARGET_ARCH_ABI := arm64-v8a
+OPENCV_LIB_TYPE := STATIC
+include $(LOCAL_PATH)/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+
+LOCAL_MODULE := NativeCapture
+
+LOCAL_CFLAGS += -Wall -Werror -Wunused -Wunreachable-code -Wno-deprecated-declarations -fexceptions
+
+LOCAL_MULTILIB := 64
+
+LOCAL_SRC_FILES := \
+    CameraCapture.cpp \
+    AudioUtil.cpp \
+    main_test.cpp \
+    YoungTest.cpp \
+    YoungTestPlus.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libbase \
+    libbinder \
+    liblog \
+    libutils \
+    libmediandk \
+    libnativewindow \
+    libcamera2ndk \
+    libOpenSLES
+
+include $(BUILD_EXECUTABLE)
